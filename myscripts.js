97271de794c215beaa8c03bf74d257ab3cbd192b
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-11 20:16:03 -0400

    Finish initial model of the game
    Currently the game works with buttoms. Next step, add feature to replay after finishing one game. Also start working on the front-end of the game

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice(){
     let Choice = Math.floor(Math.random() * 3);
     switch (Choice){
@@ -11,39 +10,75 @@ function getComputerChoice(){
     }
 }
 
-function round(){
-    let computerChoice = getComputerChoice();
-    let playerChoice = prompt('Choose your weapon').toLowerCase()
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
-
-    let result = null;
-
-    if (playerChoice === computerChoice){
-        draws += 1;
-        return (result = 'It is a draw!!')
-    }else if ((playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Rock' && computerChoice === 'Scissors') || (playerChoice === 'Scissors' && computerChoice === 'Paper')){
-        playerWins += 1;
-        return (result = `You Win! ${playerChoice} beats ${computerChoice}`)
-    } else{
-        computerWins += 1;
-        return (result = `You Lose! ${computerChoice} beats ${playerChoice}`)
-    }
-}
-
+// Display 0 in initialized scores
+let playerDivWins = document.getElementById('playerDivWins');
+let pPlayerScores = document.createElement('p');
 let playerWins = 0;
+pPlayerScores.textContent = playerWins;
+playerDivWins.appendChild(pPlayerScores);
+let computerDivWins = document.getElementById('computerDivWins');
+let pComputerScores = document.createElement('p');
 let computerWins = 0;
-let draws = 0;
+pComputerScores.textContent = computerWins;
+computerDivWins.appendChild(pComputerScores);
+
+const buttons = document.querySelectorAll('button');
+// Gets player choice
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerChoice = button.id;
+        let computerChoice = getComputerChoice();
+        let result = null;
+
+        // Checks player's and computer's choices
+        if (playerChoice === computerChoice){
+            result = 'It is a draw!!'
+        } else if ((playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Rock' && computerChoice === 'Scissors') || (playerChoice === 'Scissors' && computerChoice === 'Paper')){
+            playerWins += 1;
+            pPlayerScores.textContent = playerWins;
+            playerDivWins.appendChild(pPlayerScores);
+    
+            result = `You Win! ${playerChoice} beats ${computerChoice}`;
+        } else {
+            computerWins += 1;
+            pComputerScores.textContent = computerWins;
+            computerDivWins.appendChild(pComputerScores);
+            result = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        }
+
+        // Displays tiny window with the result
+        let results = document.getElementById('results');
+        let pResults = document.createElement('p');
+        pResults.textContent = result;
+        // Updates the result every time the player press a button
+        if (result.length) {
+            results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
+        } else{
+            results.appendChild(pResults);
+        }
+
+        if(playerWins === 5 || computerWins === 5){
+            if (playerWins === 5){
+                result = 'You win';
+                pResults.textContent = result;
+                results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
+                
+            } else {
+                result = 'You Lose';
+                pResults.textContent = result;
+                results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
+            }
+        }
+
+        buttons.forEach((button) => {
+            if (playerWins === 5 || computerWins === 5) {
+                document.getElementById("Rock").disabled = true;
+                document.getElementById("Paper").disabled = true;
+                document.getElementById("Scissors").disabled = true;
+            }
+        });
 
 
-for (let i = 0; i <= 4; i++){
-    result = round(playerWins, computerWins, draws);
-}
+    });
+});
 
-let finalResult = null
-if (playerWins > computerWins){
-    finalResult = `You Win! Stats: \n Wins: ${playerWins} \n Loses: ${computerWins} \n Draws: ${draws}`
-} else if (playerWins === computerWins){
-    finalResult = 'I cannot believe it was a tie!  Stats: \n Wins: ${playerWins} \n Loses: ${computerWins} \n Draws: ${draws}'
-} else{
-    finalResult = `You Lose with! Stats: \n Wins: ${playerWins} \n Loses: ${computerWins} \n Draws: ${draws}`
-}
