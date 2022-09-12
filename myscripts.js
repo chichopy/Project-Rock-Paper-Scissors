6c49b33a02165065003354f9df95cc1a4bf3f9af
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-12 08:01:09 -0400

    Divide myscripts.js into multiple functions for better understanding
    Prior this modification, the code was practically contain in a single function. Now the code is divided in multiple functions which facilitates the reading of the code

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,28 @@
+// Create html elements and initialized them
+let playerDivWins = document.getElementById('playerDivWins');
+let pPlayerScores = document.createElement('p');
+let playerWins = 0;
+let computerDivWins = document.getElementById('computerDivWins');
+let pComputerScores = document.createElement('p');
+let computerWins = 0;
+displayScores(playerWins, computerWins)
+
+// Gets player choice
+const buttons = document.querySelectorAll('.RPC');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerChoice = button.id;
+        let computerChoice = getComputerChoice();
+        let result = checkRound(playerChoice, computerChoice);
+        displayResult(result, playerWins, computerWins);
+        gameEnds(playerWins, computerWins)
+    });
+});
+
+// Gets computer choice
 function getComputerChoice(){
     let Choice = Math.floor(Math.random() * 3);
-    switch (Choice){
+    switch (Choice) {
         case 0:
             return (Choice = 'Rock');
         case 1:
@@ -10,75 +32,68 @@ function getComputerChoice(){
     }
 }
 
-// Display 0 in initialized scores
-let playerDivWins = document.getElementById('playerDivWins');
-let pPlayerScores = document.createElement('p');
-let playerWins = 0;
-pPlayerScores.textContent = playerWins;
-playerDivWins.appendChild(pPlayerScores);
-let computerDivWins = document.getElementById('computerDivWins');
-let pComputerScores = document.createElement('p');
-let computerWins = 0;
-pComputerScores.textContent = computerWins;
-computerDivWins.appendChild(pComputerScores);
+// Checks result of round
+function checkRound(playerChoice, computerChoice){
+    let resultRound = null;
+    // Checks player's and computer's choices
+    if (playerChoice === computerChoice){
+        resultRound = 'It is a draw!!'
+    } else if ((playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Rock' && computerChoice === 'Scissors') || (playerChoice === 'Scissors' && computerChoice === 'Paper')){
+        playerWins += 1;
+        pPlayerScores.textContent = playerWins;
+        displayScores(playerWins, computerWins)
+        resultRound = `You Win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        computerWins += 1;
+        pComputerScores.textContent = computerWins;
+        displayScores(playerWins, computerWins)
+        resultRound = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    }
+    return resultRound;
+}
 
-const buttons = document.querySelectorAll('button');
-// Gets player choice
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        let playerChoice = button.id;
-        let computerChoice = getComputerChoice();
-        let result = null;
+// display scores
+function displayScores(playerWins, computerWins){
+    pPlayerScores.textContent = playerWins;
+    playerDivWins.appendChild(pPlayerScores);
+    pComputerScores.textContent = computerWins;
+    computerDivWins.appendChild(pComputerScores);
+}
 
-        // Checks player's and computer's choices
-        if (playerChoice === computerChoice){
-            result = 'It is a draw!!'
-        } else if ((playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Rock' && computerChoice === 'Scissors') || (playerChoice === 'Scissors' && computerChoice === 'Paper')){
-            playerWins += 1;
-            pPlayerScores.textContent = playerWins;
-            playerDivWins.appendChild(pPlayerScores);
-    
-            result = `You Win! ${playerChoice} beats ${computerChoice}`;
-        } else {
-            computerWins += 1;
-            pComputerScores.textContent = computerWins;
-            computerDivWins.appendChild(pComputerScores);
-            result = `You Lose! ${computerChoice} beats ${playerChoice}`;
-        }
+// Displays results
+function displayResult(result, playerWins, computerWins){
+    // Displays tiny window with the result
+    let results = document.getElementById('results');
+    let pResults = document.createElement('p');
+    pResults.textContent = result;
+    // Updates the result every time the player press a button
+    if (result.length) {
+        results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
+    } else{
+        results.appendChild(pResults);
+    }
 
-        // Displays tiny window with the result
-        let results = document.getElementById('results');
-        let pResults = document.createElement('p');
-        pResults.textContent = result;
-        // Updates the result every time the player press a button
-        if (result.length) {
+    if(playerWins === 5 || computerWins === 5){
+        if (playerWins === 5){
+            result = 'You win';
+            pResults.textContent = result;
             results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
-        } else{
-            results.appendChild(pResults);
+            
+        } else {
+            result = 'You Lose';
+            pResults.textContent = result;
+            results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
         }
+    }
+}
 
-        if(playerWins === 5 || computerWins === 5){
-            if (playerWins === 5){
-                result = 'You win';
-                pResults.textContent = result;
-                results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
-                
-            } else {
-                result = 'You Lose';
-                pResults.textContent = result;
-                results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
-            }
+function gameEnds(playerWins, computerWins){
+    buttons.forEach(() => {
+        if (playerWins === 5 || computerWins === 5) {
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
         }
-
-        buttons.forEach((button) => {
-            if (playerWins === 5 || computerWins === 5) {
-                document.getElementById("Rock").disabled = true;
-                document.getElementById("Paper").disabled = true;
-                document.getElementById("Scissors").disabled = true;
-            }
-        });
-
-
     });
-});
 
+}
