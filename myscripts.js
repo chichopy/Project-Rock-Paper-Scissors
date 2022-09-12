c93f2aff078b4a610fa3afbfd35b39bd77296e1e
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-12 12:22:22 -0400

    Reduce redunancies in code and fix bug in result screen
    Prior this push there was a lot of repetitions inside the if-else statements of the functions. The result window now ask player to play again. In the html file were added the initial images. Next step improving CSS by adding a window that shows the player choice and the computer choice.

--- myscripts.js
+++ myscripts.js
@@ -8,14 +8,14 @@ let computerWins = 0;
 displayScores(playerWins, computerWins)
 
 // Gets player choice
-const buttons = document.querySelectorAll('.RPC');
+const buttons = document.querySelectorAll('.RPS');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         let playerChoice = button.id;
         let computerChoice = getComputerChoice();
         let result = checkRound(playerChoice, computerChoice);
         displayResult(result, playerWins, computerWins);
-        gameEnds(playerWins, computerWins)
+        //gameEnds(playerWins, computerWins)
     });
 });
 
@@ -36,19 +36,21 @@ function getComputerChoice(){
 function checkRound(playerChoice, computerChoice){
     let resultRound = null;
     // Checks player's and computer's choices
-    if (playerChoice === computerChoice){
-        resultRound = 'It is a draw!!'
-    } else if ((playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Rock' && computerChoice === 'Scissors') || (playerChoice === 'Scissors' && computerChoice === 'Paper')){
-        playerWins += 1;
-        pPlayerScores.textContent = playerWins;
-        displayScores(playerWins, computerWins)
-        resultRound = `You Win! ${playerChoice} beats ${computerChoice}`;
-    } else {
-        computerWins += 1;
-        pComputerScores.textContent = computerWins;
-        displayScores(playerWins, computerWins)
-        resultRound = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    if (playerWins < 5 && computerWins <5) {
+        if (playerChoice === computerChoice){
+            resultRound = 'It is a draw!!'
+        } else if ((playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
+                    (playerChoice === 'Scissors' && computerChoice === 'Paper')){
+            playerWins += 1;
+            pPlayerScores.textContent = playerWins;
+            resultRound = `You Win! ${playerChoice} beats ${computerChoice}`;
+        } else {
+            computerWins += 1;
+            pComputerScores.textContent = computerWins;
+            resultRound = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        }
     }
+    displayScores(playerWins, computerWins);
     return resultRound;
 }
 
@@ -67,33 +69,19 @@ function displayResult(result, playerWins, computerWins){
     let pResults = document.createElement('p');
     pResults.textContent = result;
     // Updates the result every time the player press a button
-    if (result.length) {
-        results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
-    } else{
-        results.appendChild(pResults);
-    }
-
-    if(playerWins === 5 || computerWins === 5){
+    if (playerWins < 5 && computerWins <5) {
+        if (result.length) {
+            results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
+        } else{
+            results.appendChild(pResults);
+        }
+    } else { 
         if (playerWins === 5){
-            result = 'You win';
-            pResults.textContent = result;
-            results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
-            
+            result = 'You win. Do you want to play again?';            
         } else {
-            result = 'You Lose';
-            pResults.textContent = result;
-            results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
+            result = 'You Lose. Do you want to play again?';
         }
+        pResults.textContent = result;
+        results.replaceChild(results.appendChild(pResults), results.childNodes[0]);
     }
-}
-
-function gameEnds(playerWins, computerWins){
-    buttons.forEach(() => {
-        if (playerWins === 5 || computerWins === 5) {
-            document.getElementById("Rock").disabled = true;
-            document.getElementById("Paper").disabled = true;
-            document.getElementById("Scissors").disabled = true;
-        }
-    });
-
 }
